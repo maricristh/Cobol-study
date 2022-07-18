f8bbc091c01dc40e1df8d97037a6f67ea34efcbe
maricristh <mahri.theodoro@hotmail.com>
2022-07-18 15:33:10 -0300

    Add files via upload

---
+++ prog32.cob
@@ -0,0 +1,71 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PROG32.
+      *> PRIMEIRO PROGRAMA PARA MANIPULAR ARQUIVOS, CRIANDO UM ARQUIVO
+       ENVIRONMENT DIVISION.
+
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT OPTIONAL ARQUIVO-CLIENTE
+           ASSIGN TO "clientes.txt"
+           ORGANIZATION IS SEQUENTIAL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD  ARQUIVO-CLIENTE.
+      *>     LABEL RECORDS ARE STANDARD.
+       01  CLIENTE-REGISTRO.
+           05  CLIENTE-NOME PIC X(20).
+           05  CLIENTE-ENDERECO PIC X(50).
+           05  CLIENTE-TELEFONE PIC X(15).
+           05  CLIENTE-MAIL      PIC X(30).
+
+       WORKING-STORAGE SECTION.
+
+       01  RESPOSTA PIC X.
+       
+       PROCEDURE DIVISION.
+       PROGRAM-BEGIN.
+           OPEN EXTEND ARQUIVO-CLIENTE.
+           MOVE "S" TO RESPOSTA.
+           PERFORM ADICIONA-REGISTRO
+              UNTIL RESPOSTA ="N".
+
+           CLOSE ARQUIVO-CLIENTE.
+
+       PROGRAM-DONE.
+           STOP RUN.
+
+       ADICIONA-REGISTRO.
+           MOVE SPACE TO CLIENTE-REGISTRO.
+           DISPLAY "INFORME O NOME ".
+           ACCEPT CLIENTE-NOME.
+
+           DISPLAY "INFORME O ENDERECO ".
+           ACCEPT CLIENTE-ENDERECO.
+
+           DISPLAY "INFORME O TELEFONE".
+           ACCEPT CLIENTE-TELEFONE.
+
+           DISPLAY "INFORME O EMAIL".
+           ACCEPT CLIENTE-MAIL.
+
+           WRITE CLIENTE-REGISTRO.
+           DISPLAY "-------------"
+           DISPLAY "DESEJA ADICIONAR OUTRO CLIENTE? S/N".
+           ACCEPT RESPOSTA.
+      *>     IF RESPOSTA = "S"
+      *>     PERFORM ADICIONA-REGISTRO.
+      *>     IF RESPOSTA = "N"
+      *>     STOP.
+
+
+         
+
+
+
+           
+
+
+
+
+
